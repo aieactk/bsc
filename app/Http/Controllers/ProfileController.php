127c735cccd34d01f59109bf61a66d43fa08f71c
Lim Afriyadi <lim.afriyadi.id@gmail.com>
2015-10-03 03:39:38 +0700

    Add profile actions

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 /**
  * ProfileController
@@ -10,8 +11,35 @@ use App\Http\Controllers\Controller;
  */
 class ProfileController extends Controller
 {
-    public function index() 
+    public function index($id=null) 
     {
-        return view('profile/index');
+        if($id === null) {
+            $user = \Auth::user();
+        } else {
+            $user = \App\User::findOrFail(1);
+        }
+        
+        return view('profile/index', ['user'=>$user]);
+    }
+    
+    public function save(Request $req)
+    {
+        $this->validate($req, [
+            'first_name' => 'required|max:255',
+            'last_name'  => 'required|max:255',
+            'gender'     => 'required|in:m,f',
+            'phone'      => 'required|max:15',
+            'address_1'  => 'required'
+        ]);
+        $user = \Auth::user();
+        $user->first_name = $req->input('first_name');
+        $user->last_name  = $req->input('last_name');
+        $user->gender     = $req->input('gender');
+        $user->phone      = $req->input('phone');
+        $user->address_1  = $req->input('address_1');
+        $user->address_2  = $req->input('address_2');
+        $user->save();
+        
+        return view('profile/index', ['user'=>$user]);
     }
 }
