8718473c00ffa634c188e99c1e053f095f751c24
John Elmer Semaya <elmerrxdoo@gmail.com>
2015-10-03 06:56:19 +0700

    update project page

--- resources/views/Project/project.blade.php
+++ resources/views/Project/project.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
-@section('contents')
+@section('content')
 <h1>This is List Project</h1>
 <div class="ui four stackable cards">
     @foreach ($projects as $project)
@@ -8,17 +8,11 @@
             <img src="{!! asset('image/' . $project->mainImage) !!}">
         </div>
         <div class="content">
-            <div class="header"><a href="/project/{{$project->_id}}">{{$project->title}}</a></div>
-            <div class="description">{!! str_limit($project->description, $limit = 450, $end = '...') !!}</div>
-<<<<<<< HEAD
-          </div>
-          <div class="extra content"><i class="dollar icon"></i>{{$project->goal}}</div>
-          <div class="extra content"><i class="calendar icon"></i>{{$project->duration}} Day(s)</div>
-=======
->>>>>>> d28709226ead552fdd64253c45f4869df1592967
+          <div class="header"><a href="/project/{{$project->_id}}">{{$project->title}}</a></div>
+          <div class="description">{!! str_limit($project->description, $limit = 450, $end = '...') !!}</div>
         </div>
         <div class="extra content"><i class="dollar icon"></i>{{$project->goal}}</div>
-        <div class="extra content"><i class="dollar icon"></i>{{$project->duration}}</div>
+        <div class="extra content"><i class="calendar icon"></i>{{$project->duration}} Day(s)</div>
     </div>
     @endforeach
 </div>
