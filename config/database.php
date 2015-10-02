22c1cb9da2906d1f1a1e06fb9a9216c817f50048
Lim Afriyadi <lim.afriyadi.id@gmail.com>
2015-10-03 01:33:56 +0700

    set mongo database name

--- config/database.php
+++ config/database.php
@@ -90,7 +90,7 @@ return [
           'driver'   => 'mongodb',
           'host'     => env('DB_HOST', 'localhost'),
           'port'     => env('DB_PORT', 27017),
-          'database' => env('DB_DATABASE', 'lrvmongo'),
+          'database' => env('DB_DATABASE', 'bsc'),
           'username' => env('DB_USERNAME', ''),
           'password' => env('DB_PASSWORD', ''),
           //'options' => array(
