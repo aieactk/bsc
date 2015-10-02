8b1979bd858c67080c6a82d0b23e3bcab3f25a56
Ihsan Laptop <ihsan@whatiwear.com>
2015-10-03 02:58:51 +0700

    init layout

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * HomeController
+ *
+ * @author Ihsan Kurniawan <ihsan.1x4n@gmail.com>
+ */
+class HomeController extends Controller
+{
+    public function index() 
+    {
+        return view('home/index');
+    }
+}
