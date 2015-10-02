59ec79aa048e19a2a9b50df14c397af0663d556f
Lim Afriyadi <lim.afriyadi.id@gmail.com>
2015-10-03 06:25:10 +0700

    Add member view page

---
+++ resources/views/profile/view.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('content')
+<h1>{{ $user->last_name }} Profile</h1>
+<div class="ui equal width grid">
+    <div class="six wide column">
+        <div class="ui medium image">
+            <img src="{{$user->image ? URL::to('/').$user->image : '#'}}" class="ui medium image" />
+        </div>
+    </div>
+    <div class="ten wide column">
+        <div class="ui grid">
+            <label class="six wide column">First Name</label>
+            <label class="eight wide column">{{ $user->first_name }}</label>
+            <label class="six wide column">Last Name</label>
+            <label class="eight wide column">{{ $user->last_name }}</label>
+            <label class="six wide column">Gender</label>
+            <label class="eight wide column">@if($user->gender === 'm') Male @else Female @endif</label>
+            <label class="six wide column">Phone Number</label>
+            <label class="eight wide column">{{$user->phone}}</label>
+            <label class="six wide column">Address</label>
+            <label class="eight wide column">{{$user->address_1}} {{$user->address_2}}</label>
+        </div>
+    </div>
+</div>
+@endsection
