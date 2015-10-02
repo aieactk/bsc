141adf67bf36479c13d28bab9bf6c56826ae7a01
Lim Afriyadi <lim.afriyadi.id@gmail.com>
2015-10-03 05:54:20 +0700

    Add additional user property fields

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,31 +1,48 @@
 @extends('layouts.master')
 
 @section('content')
-<form method="POST" action="/auth/register">
+<form method="POST" action="/auth/register" class="ui form">
     {!! csrf_field() !!}
-
-    <div>
-        Name
-        <input type="text" name="name" value="{{ old('name') }}">
+    <div class="field @if($errors->get('first_name')) error @endif">
+        <label>First Name</label>
+        <input type="text" name="first_name" value="{{ old('first_name') }}">
     </div>
-
-    <div>
-        Email
+    <div class="field">
+        <label>Last Name</label>
+        <input type="text" name="last_name" value="{{ old('last_name') }}">
+    </div>
+    <div class="field">
+        <label>Gender</label>
+        <select name="gender">
+            <option value="m">Male</option>
+            <option value="f">Female</option>
+        </select>
+    </div>
+    <div class="field @if($errors->get('email')) error @endif">
+        <label>Email</label>
         <input type="email" name="email" value="{{ old('email') }}">
     </div>
-
-    <div>
-        Password
+    <div class="field @if($errors->get('password')) error @endif">
+        <label>Password</label>
         <input type="password" name="password">
     </div>
-
-    <div>
-        Confirm Password
+    <div class="field">
+        <label>Confirm Password</label>
         <input type="password" name="password_confirmation">
     </div>
-
-    <div>
-        <button type="submit">Register</button>
+    <div class="field @if($errors->get('phone')) error @endif">
+        <label>Phone Number</label>
+        <input type="text" name="phone">
+    </div>
+    <div class="field @if($errors->get('address_1')) error @endif">
+        <label>Address</label>
+        <input type="text" name="address_1">
+    </div>
+    <div class="field">
+        <input type="text" name="address_2">
+    </div>
+    <div class="field">
+        <button type="submit" class="ui green button">Register</button>
     </div>
 </form>
 @endsection
