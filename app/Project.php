daa4a7a5b86607c47f0b897518fdf4f904c9981a
John Elmer Semaya <elmerrxdoo@gmail.com>
2015-10-03 09:07:03 +0700

    update Project Model

--- app/Project.php
+++ app/Project.php
@@ -2,10 +2,14 @@
 
 namespace App;
 use Jenssegers\Mongodb\Model as Eloquent;
+use Illuminate\Database\Eloquent\SoftDeletes;
 //use Illuminate\Database\Eloquent\Model;
 
 class Project extends Eloquent
 {
+  use SoftDeletes;
+
+  protected $dates = ['deleted_at'];
   protected $collection = 'project_collection';
     //
 }
