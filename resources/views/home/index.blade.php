827e171aae09c6a9de2797871bd723b305adbc2a
Ihsan Laptop <ihsan@whatiwear.com>
2015-10-04 13:38:51 +0700

    bugfix donate modal url

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,16 +9,16 @@
     <h1 class="ui inverted centered header">
         Blessed to bless others
     </h1>
-    <p class="ui centered lead">Why Blessing Supply Chain?</p>
+    <p class="ui centered lead">
+        CREDIBLE ONLINE FUNDRAISING WITH THE SOLE PURPOSE OF BLESSING OTHERS<br />
+        CURATED BY CREDIBLE ORGANIZATION / PERSON
+    </p>
     @if(!Auth::check())
     <a href="#" class="large basic inverted animated fade ui button">
         <div class="visible content">Start become blessing</div>
         <div class="hidden content">And spread God's love</div>
     </a>
     @endif
-    <div class="ui centerted image">
-        <img src="/image/bsc-front.jpg" />
-    </div>
 </div>
 @endsection
 
